9ef1d9a330bfaf5895857e5aa9336fe20119fec8
brettpenzer123 <brettpenzer123@live.co.uk>
2016-05-16 16:35:33 +0100

    Add Snow
    Snow commit

--- src/localisation/string_ids.h
+++ src/localisation/string_ids.h
@@ -2510,6 +2510,8 @@ enum {
 	STR_TRACK_LIST_NAME_FORMAT = 5813,
 	STR_TRACK_PREVIEW_NAME_FORMAT = 5814,
 
+	STR_SNOWING = 5860,
+
 	// Have to include resource strings (from scenarios and objects) for the time being now that language is partially working
 	STR_COUNT = 32768
 };
